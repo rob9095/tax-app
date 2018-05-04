027fb3e55b745862bf09aa4dd2289831b0469cfe
roberttucker9095@gmail.com <sk8terman>
2018-05-03 19:00:42 -0700

    updates to pull data from teamwork

--- client/src/store/actionTypes.js
+++ client/src/store/actionTypes.js
@@ -1,3 +1,4 @@
 export const ADD_ERROR = 'ADD_ERROR';
 export const REMOVE_ERROR = 'REMOVE_ERROR';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const LOAD_TEAMWORK_DATA = 'LOAD_TEAMWORK_DATA';
